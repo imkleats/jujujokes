1567de76f2770f4b4c60e914e6488051762fcff9
imkleats <imkleats@gmail.com>
2020-03-18 16:11:37 -0600

    Initial files

---
+++ dist/home/precache-manifest.0cfcb3cf6c9bca7ee5d84dc10fded7ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d4bb49506e0f028a4e4b14146390c56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c7da10298dec677f686",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e407bbf4e307b9583ba4",
+    "url": "/static/js/2.7e340106.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.7e340106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c7da10298dec677f686",
+    "url": "/static/js/main.b9fc5a13.chunk.js"
+  },
+  {
+    "revision": "ddd0398bb3ac6e4ae2c0",
+    "url": "/static/js/runtime-main.0de188ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
